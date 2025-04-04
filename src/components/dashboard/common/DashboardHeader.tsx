da45828765de83c3f167bbdb2b9fffca62097838
vikas-saini-7 <vikas289888@gmail.com>
2025-04-05 00:40:39 +0530

    dashboard

--- src/components/dashboard/common/DashboardHeader.tsx
+++ src/components/dashboard/common/DashboardHeader.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Bell } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 const DashboardHeader = () => {
   return (
-    <div>DashboardHeader</div>
-  )
-}
+    <header className="border-b px-6 py-3">
+      <div className="flex items-center justify-between">
+        <SidebarTrigger />
 
-export default DashboardHeader
+        <div className="flex items-center gap-4">
+          {/* Notification Dropdown */}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon">
+                <Bell className="h-5 w-5" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-80">
+              <DropdownMenuLabel>Notifications</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>New message from John Doe</DropdownMenuItem>
+              <DropdownMenuItem>Your order has been shipped</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+          {/* User Avatar Dropdown */}
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Avatar className="cursor-pointer">
+                <AvatarImage src="https://github.com/shadcn.png" />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>My Account</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Profile</DropdownMenuItem>
+              <DropdownMenuItem>Settings</DropdownMenuItem>
+              <DropdownMenuItem>Logout</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default DashboardHeader;
