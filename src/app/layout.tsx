7f1185ca15db0e4668c9fa04639322d16774b614
unknown <vaibhavthore12@gmail.com>
2025-04-05 15:25:33 +0530

    UI update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
         <main className="flex-grow">
           {children}
         </main>
-        <Footer />
         </MyProviders>
       </body>
     </html>
